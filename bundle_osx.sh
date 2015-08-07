a36e0040b11d10980f2c451766c4990a2129e45c
Seth N. Hetu <seth.hetu@gmail.com>
2015-08-07 17:34:43 -0400

    c++11 lib fixes on OS-X; added app icon.

--- bundle_osx.sh
+++ bundle_osx.sh
@@ -21,6 +21,13 @@ cp /opt/local/lib/libvorbisfile.3.dylib  LastDream.app/Contents/Frameworks/
 cp /opt/local/lib/libz.1.dylib  LastDream.app/Contents/Frameworks/
 cp /opt/local/lib/libopenal.1.dylib  LastDream.app/Contents/Frameworks/
 cp /opt/local/lib/libiconv.2.dylib  LastDream.app/Contents/Frameworks/
+cp steamworks_133b/redistributable_bin/osx32/libsteam_api.dylib  LastDream.app/Contents/Frameworks/
+
+#Make an Info file!
+cp Info.plist.in  LastDream.app/Contents/Info.plist
+
+#Copy our Icon over!
+cp ld_icon.ico LastDream.app/Contents/Resources/
 
 
 #Relink libs
@@ -35,6 +42,10 @@ install_name_tool  -change  /opt/local/lib/libvorbisfile.3.dylib @executable_pat
 install_name_tool  -change  /opt/local/lib/libz.1.dylib @executable_path/../Frameworks/libz.1.dylib   LastDream.app/Contents/MacOS/mkxp 
 install_name_tool  -change  /opt/local/lib/libopenal.1.dylib @executable_path/../Frameworks/libopenal.1.dylib   LastDream.app/Contents/MacOS/mkxp 
 install_name_tool  -change /opt/local/lib/libiconv.2.dylib @executable_path/../Frameworks/libiconv.2.dylib   LastDream.app/Contents/MacOS/mkxp 
+
+#Steam is weird
+install_name_tool  -change @loader_path/libsteam_api.dylib  @executable_path/../Frameworks/libsteam_api.dylib   LastDream.app/Contents/MacOS/mkxp
+
 
 #Copy resources
 cp -r ../LastDream_mkxp/* LastDream.app/Contents/Resources/
